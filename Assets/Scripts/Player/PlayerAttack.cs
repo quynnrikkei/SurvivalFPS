405a2be757db34effb04af6f99893e53baad0e55
Quy Nguyen Ngoc <quynn_tt@rikkeisoft.com>
2019-02-19 18:00:40 +0700

    first commit

---
+++ Assets/Scripts/Player/PlayerAttack.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttack : MonoBehaviour {
+
+    private WeaponManager weaponManager;
+
+    public float fireRate = 15f;
+    private float nextTimeToFire;
+    private float damage = 20f;
+
+    void Awake()
+    {
+        weaponManager = GetComponent<WeaponManager>();
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        WeaponShot();
+	}
+
+    void WeaponShot()
+    {
+        if (weaponManager.GetCurrentSelectedWeapon().fireType == WeaponFireType.MUTIPLE)
+        {
+            if(Input.GetMouseButton(0) && Time.time > nextTimeToFire)
+            {
+                nextTimeToFire = Time.time + 1f / fireRate;
+                weaponManager.GetCurrentSelectedWeapon().ShootAnimation();
+
+                //BulletFired();
+            }
+        }
+        else
+        {
+            if(Input.GetMouseButtonDown(0))
+            {
+                if(weaponManager.GetCurrentSelectedWeapon().tag == Tags.AXE_TAG)
+                {
+                    weaponManager.GetCurrentSelectedWeapon().ShootAnimation();
+                }
+                if(weaponManager.GetCurrentSelectedWeapon().bulletType == WeaponBulletType.BULLET)
+                {
+                    weaponManager.GetCurrentSelectedWeapon().ShootAnimation();
+                    //BulletFired();
+                }
+                else
+                {
+                     
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private CharacterController character_controller;
+    private Vector3 move_Direction;
+
+    public float speed = 5f;
+    private float gravity = 20f;
+
+    public float jump_Force = 10f;
+    private float vertical_Velocity;
+
+    void Awake()
+    {
+        character_controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveThePlayer();
+    }
+
+    void MoveThePlayer()
+    {
+        move_Direction = new Vector3(Input.GetAxis(Axis.HORIZONTAL), 0f,
+                                     Input.GetAxis(Axis.VERTICAL));
+
+        move_Direction = transform.TransformDirection(move_Direction);
+        move_Direction *= speed * Time.deltaTime;
+
+        ApplyGravity();
+
+        character_controller.Move(move_Direction);
+    }
+
+    void ApplyGravity()
+    {
+        if (character_controller.isGrounded)
+        {
+            vertical_Velocity -= gravity * Time.deltaTime;
+            PlayerJump();
+        }
+        else
+        {
+            vertical_Velocity -= gravity * Time.deltaTime;
+        }
+        move_Direction.y = vertical_Velocity * Time.deltaTime;
+    }
+
+    void PlayerJump()
+    {
+        if (character_controller.isGrounded && Input.GetKeyDown(KeyCode.Space))
+        {
+            vertical_Velocity = jump_Force;
+        }
+    }
+}
